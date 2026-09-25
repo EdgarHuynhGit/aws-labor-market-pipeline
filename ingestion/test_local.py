c4ac6152e280b1b55c8ec16be7f219f5bb95f27e
EdgarHuynhGit <huynhedgar@gmail.com>
2026-09-25 12:35:55 -0500

    test

--- ingestion/test_local.py
+++ ingestion/test_local.py
@@ -1,10 +1,10 @@
 import os
 os.environ["RAW_BUCKET"] = "labor-market-pipeline-raw-dev"
-os.environ["ADZUNA_APP_ID"] = "your_app_id"
-os.environ["ADZUNA_APP_KEY"] = "your_app_key"
+os.environ["ADZUNA_APP_ID"] = "0f1bbf01"
+os.environ["ADZUNA_APP_KEY"] = "984e8589ae95326991225fd04b295209"
 os.environ["ADZUNA_QUERY"] = "data analyst"
 
-from lambda_function import handler1
+from lambda_function import handler
 
 result = handler({}, None)
-print(result)
+print(result)  
